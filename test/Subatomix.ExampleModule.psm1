11600fbc61f835efe59af8f6a83bbcc6f6121a3f
Jeff Sharp <public@sharpjs.net>
2021-04-18 12:44:51 -0700

    Prevent movement of goalpost.
    That's safer when using this as a project template.

--- test/Subatomix.ExampleModule.psm1
+++ test/Subatomix.ExampleModule.psm1
@@ -18,7 +18,7 @@
 #Requires -PSEdition Core
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues.Clear()
-Set-StrictMode -Version Latest
+Set-StrictMode -Version 3.0
 
 # Read all .ps1 files
 Join-Path $PSScriptRoot Commands *.ps1 -Resolve | ForEach-Object { . $_ }
